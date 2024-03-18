5284cbf84881e0dde02f4c6cc70613cee34de7bf
Kanmi Obasa <kanmi@konfirmed.com>
2024-03-18 03:40:53 -0700

    clean up some more

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Freemium  from "@/components/freemium";
+// import Freemium  from "@/components/freemium";
+import { LH } from "@/components/lh";
 
 export default function Page() {
   return (
-    <Freemium />
+    // <Freemium />
+    <LH />
   );
 }
